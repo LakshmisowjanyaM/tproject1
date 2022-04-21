6cf3fd2efacb3e6fec11686f10d97ff9dbb041cd
LakshmisowjanyaM <mlsowjanyasri@gmail.com>
2022-04-21 09:40:05 +0000

    UI5LIST

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,38 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,JSONModel) {
         "use strict";
 
         return Controller.extend("tproject1.controller.View1", {
             onInit: function () {
-
+               this.getView().setModel(new JSONModel([]), "oProductTable");
+               this._getData();
+            },
+            _getData:function(){
+                var serviceUrl1 = this.getView().getModel().sServiceUrl;
+			    var sPath1 = "/product";
+			    var aUrl = serviceUrl1 + sPath1;
+                var that = this;
+			$.ajax({
+				url: aUrl,
+				method: "GET",
+				async: true,
+				contentType: "application/json",
+				success: function (oData) {
+					console.log(oData, "oj");
+                    that.getView().getModel("oProductTable").setData(oData.value);
+                    that.getView().getModel("oProductTable").refresh(true);
+					
+				},
+				error: function (oError) {
+					console.log("Error in fetching user data");
+				}
+			});
             }
         });
     });
